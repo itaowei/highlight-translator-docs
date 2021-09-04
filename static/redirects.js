15a30dc486ed3b951e8ffc6d331535aa7a143fd2
lmk123 <me@limingkai.cn>
2021-09-05 02:56:43 +0800

    fix: 临时修复网站地址不能跳转的问题

--- static/redirects.js
+++ static/redirects.js
@@ -1,3 +1,8 @@
+// 升级到 docusaurus 2.0.0-beta.6 后，生产环境里点击链接都不能正常跳转了，而且控制台会报
+// window.ga is not a function 的错误。已经确认过无论是否开启谷歌分析的插件都会报这个错，
+// 给 window.ga 补个函数就修复了
+window.ga = function () {}
+
 const { search, pathname, hostname } = location
 
 const hasInviteCode = search.startsWith('?i=')
